3ef649d804159203600dda86947a5baa5008fc95
Diogo Costa <diogo.alex.nc@gmail.com>
2023-10-15 22:46:05 +0100

    Profiles now have dynamic shadows

--- src/app/components/mainpage/others/Profile.js
+++ src/app/components/mainpage/others/Profile.js
@@ -9,12 +9,17 @@ import { useState } from "react";
 export default function Profile(props) {
   const avatarScale = "10.44rem";
 
+  const boxShadowProp = {
+    enabled: "0px 5px 5px 0px rgba(0,0,0,0.5)",
+    disabled: "0px 0px 0px 0px rgba(0,0,0,0)",
+  };
+
   const avatarStyle = {
     width: avatarScale,
     height: avatarScale,
     position: "relative",
     bottom: "5.2rem",
-    boxShadow: "0px 5px 5px 0px rgba(0,0,0,0.5)",
+    boxShadow: boxShadowProp.enabled,
   };
 
   const buttonStyle = {
@@ -32,7 +37,13 @@ export default function Profile(props) {
   let amISelected = props.id === props.showFilesId ? true : false;
 
   return (
-    <Box sx={{ opacity: amISelected ? 0.3 : 1 }} className="ProfileCard">
+    <Box
+      sx={{
+        opacity: amISelected ? 0.3 : 1,
+        boxShadow: amISelected ? boxShadowProp.disabled : boxShadowProp.enabled,
+      }}
+      className="ProfileCard"
+    >
       <Avatar
         sx={avatarStyle}
         src={props.img}
